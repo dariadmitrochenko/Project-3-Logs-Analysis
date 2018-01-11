26133c992c088381df0b5e4e5454a5b92b3c3984
Daria-Dmitrochenko <dmtrochenko.d@gmail.com>
2018-01-11 13:12:24 -0500

    Reviewer's suggestions added

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ by **[Daria Dmitrochenko](https://github.com/dariadmitrochenko)**
 4. Create views:
   * Connect to the database by typing `psql -d news`
   * Create view **article_views** by copying and pasting this:
-`CREATE VIEW article_views AS SELECT title, author, name, COUNT(log.path) AS views FROM articles, log, authors WHERE log.path LIKE ('%' || articles.slug) AND authors.id = articles.author GROUP BY articles.title, articles.author, authors.name ORDER BY views desc;`
+`CREATE VIEW article_views AS SELECT title, author, name, COUNT(log.path) AS views FROM articles, log, authors WHERE log.path = ('/article/' || articles.slug) AND authors.id = articles.author GROUP BY articles.title, articles.author, authors.name ORDER BY views desc;`
   * Create view **errors** by copying and pasting this: `CREATE VIEW errors AS SELECT date(time),100.0*sum(case log.status WHEN '200 OK' then 0 else 1 end)/count(log.status) as error_percentage FROM log group by date(time) ORDER BY error_percentage desc;`
 
-5. Type `\q` to return to your *vagrant* directory and type `python newsdata.py` to run the python program. Your output in the Terminal should look like this:
+5. Type `\q` to return to your *vagrant* directory and type `python3 newsdata.py` to run the python program. Your output in the Terminal should look like this:
 
 ```
 1.Top three articles are:
@@ -28,14 +28,16 @@ by **[Daria Dmitrochenko](https://github.com/dariadmitrochenko)**
 "Bad things gone, say good people" - 170098 views
 
 
-2.Top three authors are:
+2.Top authors are:
 
 Ursula La Multa - 507594 views
 Rudolf von Treppenwitz - 423457 views
 Anonymous Contributor - 170098 views
+Markoff Chaney - 84557 views
 
 
 3.Days with more than 1% of requests lead to errors are:
 
-2016-07-17 - 2.2626862468027260% errors
+July 17, 2016 - 2.26%
+
 ```
--- newsdata.py
+++ newsdata.py
@@ -1,4 +1,4 @@
-#! /usr/local/bin/python3
+#! /usr/bin/python3
 import psycopg2
 
 
@@ -6,14 +6,16 @@ top_three_articles = """select title, views
 from article_views
 limit 3;"""
 
-top_three_authors = """select authors.name,sum(article_views.views) as views
+top_authors = """select authors.name as author_name,
+sum(article_views.views) as views
 from article_views,authors
 where authors.id = article_views.author
-group by authors.name 
-order by views desc
-limit 3;"""
+group by author_name
+order by views desc;"""
 
-day_with_most_errors = """select * from errors
+day_with_most_errors = """select to_char(date,'FMMonth FMDD, YYYY') as date,
+round(error_percentage, 2) as error_percentage
+from errors
 where error_percentage > 1.0;"""
 
 
@@ -22,24 +24,24 @@ def get_results(sql_query):
     c = db.cursor()
     c.execute(sql_query)
     results = c.fetchall()
-    return results
     db.close()
+    return results
 
 
 if __name__ == '__main__':
     print("\n1.Top three articles are:\n")
     articles = (get_results(top_three_articles))
-    for row in articles:
-        print("\"%s\" - %s views" % (row[0], row[1]))
+    for title, views in articles:
+        print('"{}" - {} views'.format(title, views))
 
-    print("\n\n2.Top three authors are:\n")
-    authors = (get_results(top_three_authors))
-    for row in authors:
-        print("%s - %s views" % (row[0], row[1]))
+    print("\n\n2.Top authors are:\n")
+    authors = (get_results(top_authors))
+    for author_name, views in authors:
+        print('{} - {} views'.format(author_name, views))
 
     print("\n\n3.Days with more than 1% of requests lead to errors are:\n")
     errors = (get_results(day_with_most_errors))
-    for row in errors:
-        print("%s - %s%% errors" % (row[0], row[1]))
+    for date, error_percentage in errors:
+        print('{} - {}%'.format(date, error_percentage))
 
     print("\n")
